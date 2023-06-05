29ec04cc5080032ee699f16575f995d0df89c4cc
d4m1ts <damit5@protonmail.com>
2023-06-05 15:49:59 +0800

    23-06-05 15:49:59 主题更新同步，增加目录跟随滚动

--- gitbook/gitbook-plugin-theme-d4t/script.js
+++ gitbook/gitbook-plugin-theme-d4t/script.js
@@ -13,13 +13,19 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
 
     //生成内容导航
     function generateSectionNavigator(){
-        $('.page-inner .markdown-section').find('h1,h2,h3').each(function(){
+        $('.page-inner .markdown-section').find('h1,h2,h3,h4,h5').each(function(){
             let cls = 'anchor-h1';
             if($(this).is('h2')){
                 cls = 'anchor-h2';
             }
             if($(this).is('h3')){
                 cls ='anchor-h3';
+            }
+            if($(this).is('h4')){
+                cls ='anchor-h4';
+            }
+            if($(this).is('h5')){
+                cls ='anchor-h5';
             }
             const text = $(this).text();
             const href = $(this).attr("id");
@@ -86,6 +92,67 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
       }
     };
 
+    // 增加内容导航栏目跟随滚动
+    // 获取第一个可见标题的函数
+    function getFirstVisibleHeading() {
+      // 选择所有的 h1、h2、h3 标题元素
+      var headings = document.querySelectorAll("h1, h2, h3, h4, h5");
+
+      // 遍历标题元素列表
+      for (var i = 0; i < headings.length; i++) {
+        var heading = headings[i];
+
+        // 判断标题元素是否在可视区域内（距离可视区域顶部的距离小于等于阈值）
+        if (isElementInViewport(heading, 20)) {
+          // 如果是，则返回标题元素的文本内容
+          return heading.textContent;
+        }
+      }
+
+      // 如果没有可见的标题元素，则返回空字符串
+      return "";
+    }
+
+    // 判断元素是否在可视区域内的函数
+    function isElementInViewport(element, percentage) {
+      // 获取元素的位置信息
+      var rect = element.getBoundingClientRect();
+      // 获取窗口的高度
+      var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      var elementTop = rect.top;
+      var elementBottom = rect.bottom;
+      // 根据给定的百分比计算阈值
+      var threshold = windowHeight * (percentage / 100);
+
+      // 判断元素是否在可视区域内（距离可视区域顶部或底部的距离小于等于阈值）
+      return (elementTop >= 0 && elementTop <= threshold) || (elementBottom >= 0 && elementBottom <= threshold);
+    }
+
+    // 每次滚动后更新
+    function scrollCheck() {
+        // 当滚动 book-body 或 body-inner 元素时触发事件
+        $('.book-body, .body-inner').on('scroll', function() {
+          // 获取第一个可见标题
+          var firstVisibleHeading = getFirstVisibleHeading();
+          // 如果存在可见标题
+          if (firstVisibleHeading !== "") {
+            // 根据标题生成锚点的 ID
+            var anchorId = 'an_' + firstVisibleHeading;
+            // 根据锚点的 ID 获取锚点元素
+            var anchorElement = document.getElementById(anchorId);
+
+            // 如果锚点元素存在
+            if (anchorElement !== null) {
+              // 移除已选中的锚点样式
+              $('.anchor-text, .selected').removeClass('selected');
+              // 添加选中样式到当前锚点元素
+              $(anchorElement).addClass('selected');
+            }
+          }
+        });
+    }
+
+
     gitbook.events.on('start', function() {
 
     });
@@ -94,5 +161,6 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
         setBase();
         generateSectionNavigator();
         mobileAdaptation();
+        scrollCheck();
     });
 });

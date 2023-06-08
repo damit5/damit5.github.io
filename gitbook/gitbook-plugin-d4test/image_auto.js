e78c490a7abaa3c8363ca801fc8024f668388d7d
d4m1ts <damit5@protonmail.com>
2023-06-08 16:25:32 +0800

    23-06-08 16:25:32 修复主题漏洞，更新逻辑漏洞和致谢

--- gitbook/gitbook-plugin-d4test/image_auto.js
+++ gitbook/gitbook-plugin-d4test/image_auto.js
@@ -1,27 +1,27 @@
 require(["gitbook"], function(gitbook) {
   // 加载完成后更新
   gitbook.events.bind("page.change", function(e, pluginConfig) {
-    // 获取所有的<img>元素
-    let images = document.getElementsByTagName('img');
+    // // 获取所有的<img>元素
+    // let images = document.getElementsByTagName('img');
 
-    // 遍历每个图片并设置缩小效果
-    for (let i = 0; i < images.length; i++) {
-      let img = images[i];
+    // // 遍历每个图片并设置缩小效果
+    // for (let i = 0; i < images.length; i++) {
+    //   let img = images[i];
 
-      // 检查图片地址是否包含"logo"或"shields.io"
-      if (/logo|shields\.io|netlify|robohash\.org|gzh_[0-1]\.png|\.svg/.test(img.src)) {
-        continue; // 不缩小带有"logo"或"shields.io"的图片
-      }
+    //   // 检查图片地址是否包含"logo"或"shields.io"
+    //   if (/logo|shields\.io|netlify|robohash\.org|gzh_[0-1]\.png|\.svg/.test(img.src)) {
+    //     continue; // 不缩小带有"logo"或"shields.io"的图片
+    //   }
       
-      // 图片缩小50%
-      img.style.zoom = '50%';
-      // 居中展示
-      img.style.display = 'block';
-      img.style.marginLeft = 'auto';
-      img.style.marginRight = 'auto';
-      // 增加阴影
-      img.style.boxShadow = '0px 0px 5px rgba(0, 0, 0, 0.5)';
-    }
+    //   // 图片缩小50%
+    //   img.style.zoom = '50%';
+    //   // 居中展示
+    //   img.style.display = 'block';
+    //   img.style.marginLeft = 'auto';
+    //   img.style.marginRight = 'auto';
+    //   // 增加阴影
+    //   img.style.boxShadow = '0px 0px 5px rgba(0, 0, 0, 0.5)';
+    // }
 
     // 获取当前页面中所有的video标签
     let videoTags = document.querySelectorAll('video');
